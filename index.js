519360555f8c8b1f980e1a28cebe724d2dac620b
Jason Pierce <jpierce@surgeforward.com>
2017-02-07 19:18:27 -0800

    Add copyright header

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/*
+ * node-teradata
+ * Copyright (c) 2017 2Toad, LLC
+ * https://github.com/2Toad/node-teradata
+ *
+ * Version: 1.2.0
+ * License: MIT
+ */
+
 var jinst = require('jdbc/lib/jinst');
 var Pool = require('jdbc/lib/pool');
 var Promise = require('bluebird');
