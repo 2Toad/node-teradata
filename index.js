20729e17ca93ead3111a4d7a8340bbffe4cc63f2
Jason Pierce <jpierce@surgeforward.com>
2017-02-15 13:36:18 -0800

    Add index to reference lib/teradata

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * node-teradata
+ * Copyright (c) 2017 2Toad, LLC
+ * https://github.com/2Toad/node-teradata
+ *
+ * Version: 1.3.1
+ * License: MIT
+ */
+
+ module.exports = require('./lib/teradata');
